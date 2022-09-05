563e9c67431f320b15b5645067d29d19bcb85aab
unknown <arrispower@yandex.ru>
2022-09-05 15:14:34 +0500

    проверка

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+int FindBiggerDigit(int number)
+{
+    int ed = number % 10;
+    int dec = number /10;
+
+    int result;
+    if(ed > dec) result = ed;
+    else result = dec;
+
+    return result; 
+}
+
+int randNumber = new Random().Next(10, 1000);
+int biggerDigit = FindBiggerDigit(randNumber);
+
+Console.WriteLine($"Bigger digit of {randNumber} is {biggerDigit}");
+*/
+
+static void Main(string[] args)
+{
+    int n = 534534623;
+    int d = GetSecondDigit(n);
+    Console.WriteLine(d);
+    Console.ReadKey();
+}
